0c230c0da35391abb4bc78e440d0e5435e896a4a
Randee Orion <randeegood@gmail.com>
2019-11-09 14:50:14 -0800

    files linked to each other so we can check sales page.

---
+++ js/app.js
@@ -0,0 +1,36 @@
+'us strict';
+
+var Seattle = document.getElementById('Seattle');
+
+var liEl = document.createElement('li');
+
+liEl.textContent = 'proof of life';
+
+console.log(liEl);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var createStore = function(location, min, max, avg) {
+//     this.location = location;
+//     this.min = min;
+//     this.max = max;
+//     this.avg = avg;
+//     console.log(`Store location: ${location}, Minimum hourly customer ${min}, Maximum hourly customer: ${max}, Average customer ${avg};`)
+// }
+
+// createStore(Seattel,5,3,7);
