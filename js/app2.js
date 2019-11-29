2011d331110c2913999f1a13fb97a11e79e3eae8
Randee Orion <randeegood@gmail.com>
2019-11-29 11:04:25 -0800

    footer row is almost complete

--- js/app2.js
+++ js/app2.js
@@ -2,13 +2,14 @@
 
 var tableLocation = document.getElementById('storeSales');
 var allStores = [];
+console.log(allStores);
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
 
 
 
 function CreateStore(name, minCustomer, maxCustomer, avgCookie) {
   this.name = name;
-  // console.log(name);
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
   this.avgCookie = avgCookie;
@@ -18,28 +19,18 @@ function CreateStore(name, minCustomer, maxCustomer, avgCookie) {
   this.calCustomersEachHour();
   this.calcCookiesSoldEachHour();
   allStores.push(this);
-
+  //this.createFooterRow();
   this.generateTableBody();
 }
 
-//this needs work.
 
 
 function randomNumber(min, max) {
   return Math.round(Math.floor(Math.random() * (max - min)) + min);
 }
 
-// function addElement (childElType, childContent, parentEl) {
-//   var childElement = document.createElement(childElement);
-//   var childContent = 
-//   //Need the rest o this from front row
-
 
 CreateStore.prototype.calCustomersEachHour = function () {
-  //generate a random number
-  //store it in its array
-  //create a random number for each hour
-  //push the random number into the customersEachHour
   for (var i = 0; i < hours.length; i++) {
     var randomCustomerForOneHour = randomNumber(this.minCustomer, this.maxCustomer);
     this.customersEachHour.push(randomCustomerForOneHour);
@@ -51,79 +42,70 @@ CreateStore.prototype.calcCookiesSoldEachHour = function () {
     var cookiesForOneHour = this.avgCookie * this.customersEachHour[i];
     cookiesForOneHour = Math.round(cookiesForOneHour);
     this.cookiesSoldEachHour.push(cookiesForOneHour);
-
     this.totalCookiesForTheDay += cookiesForOneHour;
-
   }
 };
 
 
 function generateTableHeader() {
-  //make a tr
   var trEl = document.createElement('tr');
-  //make a td filled with Name and append to tr
   var thEl = document.createElement('th');
   thEl.textContent = 'Store Name';
   trEl.appendChild(thEl);
-  //make a loop to create th for each hour and append to tr
   for (var i = 0; i < hours.length; i++) {
     thEl = document.createElement('th');
     thEl.textContent = hours[i];
     trEl.appendChild(thEl);
   }
-  //make a td to hold the word totals and append to tr
   thEl = document.createElement('th');
   thEl.textContent = 'Total Daily Cookies';
   trEl.appendChild(thEl);
   tableLocation.appendChild(trEl);
 }
-
 generateTableHeader();
 
 CreateStore.prototype.generateTableBody = function () {
-  //create tr append to table
   var trEl = document.createElement('tr');
   tableLocation.appendChild(trEl);
-  //create a td fill with this.name and append to tr
   var thEl = document.createElement('th');
   thEl.textContent = this.name;
   trEl.appendChild(thEl);
-  //create a for loop over this.cookiesSoldEachHour create a td and fill with cookiesSoldEachHour[i] then append to tr
   for (var i = 0; i < hours.length; i++) {
     var tdEl = document.createElement('td');
     tdEl.textContent = this.cookiesSoldEachHour[i];
+
     trEl.appendChild(tdEl);
   }
-  //create a td fill with this.totalCookiesForTheDay and append to tr
   tdEl = document.createElement('td');
   tdEl.textContent = this.totalCookiesForTheDay;
   trEl.appendChild(tdEl);
-}
+};
 //create footer row
-var createFooterRow = function () {
-  //create trEl and tdEl Variables  
+CreateStore.prototype.createFooterRow = function () {  
   var trEl = document.createElement('tr');
   var tdEl = document.createElement('td');
-  //write 'hourly totals' in the first td and append to trEl
   tdEl.textContent = 'Hourly Totals: ';
   trEl.appendChild(tdEl);
-  //create a for loop that loops over this.hours.length
+  console.log('hourly totals; ', tdEl);
+
   for (var i = 0; i < hours.length; i++) {
     var storeHourlyTotals = 0;
-    //create a variable storesHourlyTotals to 0   
-    //create a second for loop, j, that loops over this.allStores
-    for (var j = 0; j < allStores.length; i++){storeHourlyTotals += allStores[j].cookiesSoldEachHour;}
+    tdEl = document.createElement('td');
+
+    for (var j = 0; j < allStores.length; j++){
+      storeHourlyTotals += allStores[j].cookiesSoldEachHour[i];}
+    console.log('hello');
     //tdEl text content storesHourlyTotals
     tdEl.textContent = storeHourlyTotals;
     trEl.appendChild(tdEl);
+  
   }
-
-  tdEl.textContent = storeHourlyTotals;
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.totalCookiesForTheDay;
   trEl.appendChild(tdEl);
   tableLocation.appendChild(trEl);
 };
 
-createFooterRow();
 
 new CreateStore('Seattle', 23, 65, 6.3);
 new CreateStore('Tokyo', 3, 24, 1.2);
@@ -132,11 +114,7 @@ new CreateStore('Lima', 2, 16, 4.6);
 new CreateStore('Paris', 20, 38, 2.3);
 
 
-// for (var i = 0; i < allStores.length; i++) {
-//   allStores[i].generateTableBody();
-// }
-
-//bookmark the DOM at userForm and addEventListener 
+CreateStore.prototype.createFooterRow();
 
 
 
